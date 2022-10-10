46f0ceeecdc2026b57deaa69303b7d3b56d0ed10
Ricardo Rocha <me@rrocha.uk>
2022-10-11 00:17:34 +0100

    :rocket: Initiate a react frontend;

---
+++ frontend-react/src/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/v1/photo/all', {
+      headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_BEARER_TOKEN}`
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => setPhotos(data));
+  }, []);
+
+  return <div>
+    {photos.map((photo: any) =>
+      // (<div key={photo.filename}>{photo.fullPath}</div>)
+      (<img src={'http://localhost:4000/api/v1/photo/'+photo.fullPath} key={photo.filename} alt={photo.filename}/>)
+    )}
+  </div>;
+}
+
+export default App;
