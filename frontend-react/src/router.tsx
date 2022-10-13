f7408fb9446ea664b444b6c7befd6c92fc0d9656
Ricardo Rocha <me@rrocha.uk>
2022-10-13 09:00:01 +0100

    * some authguard poc;

--- frontend-react/src/router.tsx
+++ frontend-react/src/router.tsx
@@ -1,7 +1,7 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import RequestAccess from "./pages/RequestAccess";
+import { createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import RequestAccess from './pages/RequestAccess';
 
 export const router = createBrowserRouter([
   {
@@ -10,10 +10,30 @@ export const router = createBrowserRouter([
   },
   {
     path: '/login',
-    element: <Login/>
+    element: <Login/>,
+    loader: nonAuthedGuard,
   },
   {
     path: '/request-access',
-    element: <RequestAccess/>
+    element: <RequestAccess/>,
+    loader: nonAuthedGuard,
   }
 ]);
+
+// if there is a token then the user is assumed "authenticated"
+// then we redirect to homepage
+function nonAuthedGuard(): boolean {
+  const token = localStorage.getItem('access-token');
+
+  return token !== null;
+  // return new Promise((resolve, reject) => {
+  //   fetch('http://localhost:4000/api/v1/user/info', {
+  //     headers: {
+  //       'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJyaWNkb3RuZXQiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTcxNzIzOTAyMn0.oRq6K2uvz0B2e7BEuqIYfJ3whumVhDmm5j4Z3mEsZAM'
+  //     }
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => resolve(data))
+  //     .catch((err) => console.error(err));
+  // });
+}
