bfe807be5e4eb69647fa72f1a65027018a06c9c1
Ricardo Rocha <me@rrocha.uk>
2022-10-28 23:06:09 +0100

    PH-e006e: Create user Dashboard
    * new nav link (/dashboard);
    * new Dashboard page;
    * new router entry for the dashboard;

---
+++ frontend-react/src/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+import UserLayout from '../layouts/UserLayout';
+
+interface DashboardPropsInterface {
+
+}
+
+export default function Dashboard(props: DashboardPropsInterface) {
+  return (
+    <UserLayout>
+      <div>user dashboard page ayyyyoooo</div>
+    </UserLayout>
+  );
+}
