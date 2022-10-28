bf36b5166163735f4847e82b4d1a5625ae2a87a7
Ricardo Rocha <me@rrocha.uk>
2022-10-28 22:34:42 +0100

    * updated the RequestAccess page;

--- frontend-react/src/pages/RequestAccess.tsx
+++ frontend-react/src/pages/RequestAccess.tsx
@@ -1,9 +1,13 @@
 import GuestLayout from '../layouts/GuestLayout';
+import { Link } from 'react-router-dom';
 
 function RequestAccess() {
   return (
     <GuestLayout>
-      <div>This is the RequestAccess page...</div>
+      <div className="flex flex-col space-y-4 text-center">
+        <span>Not accepting any requests right now. Sorry :(</span>
+        <span><Link to="/login">Go back</Link></span>
+      </div>
     </GuestLayout>
   );
 }
